db448870d93c7b4f9925d27497920d6f6e1d272b
かずのこ <k42un0k0.put1put1@gmail.com>
2022-10-11 17:36:01 +0900

    refactor ExampleItems.java

--- src/main/java/com/example/examplemod/item/ExampleItems.java
+++ src/main/java/com/example/examplemod/item/ExampleItems.java
@@ -3,6 +3,7 @@ package com.example.examplemod.item;
 import com.example.examplemod.block.ExampleBlocks;
 import com.example.examplemod.ExampleMod;
 import com.example.examplemod.item.custom.Firestone;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,55 +18,42 @@ public class ExampleItems {
             new Item(new Item.Properties()
                     .tab(ExampleItemGroup.TAB)));
 
-    public static final RegistryObject<Item> TITANIUM_BLOCK = ITEMS.register("titanium_block", () ->
-            new BlockItem(ExampleBlocks.TITANIUM_BLOCK.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> TITANIUM_BLOCK = registerBlock("titanium_block", ExampleBlocks.TITANIUM_BLOCK);
 
     public static final RegistryObject<Item> AMETHYST = ITEMS.register("amethyst", () ->
             new Item(new Item.Properties()
                     .tab(ExampleItemGroup.TAB)));
 
-    public static final RegistryObject<Item> AMETHYST_BLOCK = ITEMS.register("amethyst_block", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_BLOCK.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_BLOCK = registerBlock("amethyst_block", ExampleBlocks.AMETHYST_BLOCK);
 
-    public static final RegistryObject<Item> AMETHYST_ORE = ITEMS.register("amethyst_ore", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_ORE.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_ORE =registerBlock("amethyst_ore", ExampleBlocks.AMETHYST_ORE);
 
-    public static final RegistryObject<Item> AMETHYST_STAIRS = ITEMS.register("amethyst_stairs", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_STAIRS.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_STAIRS =registerBlock("amethyst_stairs", ExampleBlocks.AMETHYST_STAIRS);
 
-    public static final RegistryObject<Item> AMETHYST_WALL = ITEMS.register("amethyst_wall", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_WALL.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_WALL =registerBlock("amethyst_wall", ExampleBlocks.AMETHYST_WALL);
 
-    public static final RegistryObject<Item> AMETHYST_DOOR = ITEMS.register("amethyst_door", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_DOOR.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_DOOR = registerBlock("amethyst_door", ExampleBlocks.AMETHYST_DOOR);
 
-    public static final RegistryObject<Item> AMETHYST_TRAP_DOOR = ITEMS.register("amethyst_trap_door", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_TRAP_DOOR.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_TRAP_DOOR = registerBlock("amethyst_trap_door", ExampleBlocks.AMETHYST_TRAP_DOOR);
 
-    public static final RegistryObject<Item> AMETHYST_PANE = ITEMS.register("amethyst_pane", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_PANE.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_PANE = registerBlock("amethyst_pane", ExampleBlocks.AMETHYST_PANE);
 
-    public static final RegistryObject<Item> AMETHYST_SLAB = ITEMS.register("amethyst_slab", () ->
-            new BlockItem(ExampleBlocks.AMETHYST_SLAB.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> AMETHYST_SLAB = registerBlock("amethyst_slab", ExampleBlocks.AMETHYST_SLAB);
 
     public static final RegistryObject<Item> FIRESTONE = ITEMS.register("firestone", () ->
             new Firestone(new Item.Properties().durability(8)
                     .tab(ExampleItemGroup.TAB)));
 
-    public static final RegistryObject<Item> FIRESTONE_BLOCK = ITEMS.register("firestone_block", () ->
-            new BlockItem(ExampleBlocks.FIRESTONE_BLOCK.get(), new Item.Properties()
-                    .tab(ExampleItemGroup.TAB)));
+    public static final RegistryObject<Item> FIRESTONE_BLOCK = registerBlock("firestone_block", ExampleBlocks.FIRESTONE_BLOCK);
+
+    private static RegistryObject<Item> registerBlock(String name, RegistryObject<Block> blockRegistryObject) {
+        return ITEMS.register(name, () ->
+                new BlockItem(blockRegistryObject.get(), new Item.Properties().tab(ExampleItemGroup.TAB)));
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
+
+
 }
